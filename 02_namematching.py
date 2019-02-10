571b945107ae2a134736d196b5e947fc03e84951
LucasStevenson <45473315+LucasStevenson@users.noreply.github.com>
2019-02-10 00:22:27 -0800

    comments and light fixes. still yet to make the improved txt file

--- 02_namematching.py
+++ 02_namematching.py
@@ -3,32 +3,32 @@
 # Or just use a python function for it
 from collections import defaultdict 
 
-# What happens if no defaultdict? 
+# What happens IF no defaultdict? 
 # Have to check if dictionary[key] is null? 
 # If NULL, create a list
 # append
 
 with open("fall_users.txt") as f: # Opening the file
-    line = f.readline()
-    dictionary = defaultdict(list)
-    reverse_dict = defaultdict(list)
+    line = f.readline() # reads the file
+    dictionary = defaultdict(list) # a new dict for the firstname => lastname
+    reverse_dict = defaultdict(list) # a new dict for lastname => firstname
     while line:
-        res = line.rstrip().split("\t")
-        rc1 = res[0].capitalize()
-        rc2 = res[1].capitalize()
+        res = line.rstrip().split("\t") # strips the newline at the end of every name entry, and also strips the tab between the first and last names
+        rc1 = res[0].capitalize() # .capitalize() makes the first letter cap and everything else lower
+        rc2 = res[1].capitalize() # takes the second value of the array (lastname)
         # if dictionary[rc1] is NULL or reverse_dict[rc2] is NULL, make a list at those values. Only if no defaultdict
-        dictionary[rc1].append(rc2)
-        reverse_dict[rc2].append(rc1)
-        line = f.readline()
+        dictionary[rc1].append(rc2) # appends lastname -> value of the dict
+        reverse_dict[rc2].append(rc1) # appends firstname -> value of the dict
+        line = f.readline() # iterates thru the file and all the vals
 
 # have key, value, and dictionary 
 # What need is: First name(Followed by occurrence if > 1) : linked last names)
 print("FIRST NAME(REPEATS) : LAST NAMES\n")
 for k in dictionary: #for the key in dictionary
     v = dictionary[k] #value
-    length = len(dictionary[k]) #length of the last names
+    length = len(dictionary[k]) #length of the last names (how many there are)
     if length > 1: #checks whether the length of the last names array is greater than one
-        print(k + "(" + str(length) + "):" + str(v))
+        print(k + "(" + str(length) + "): " + str(v))
 
 print("\n\n")
 # how many people share the input name + what last names are 
--- 02_numtypes.py
+++ 02_numtypes.py
@@ -1,5 +1,4 @@
-print("Hello, how old are you?")
-xString = int(input())
+xString = int(input("Hello, how old are you?: "))
 print("In five years you will be " + str(xString+5))
 print("Your age times 10 is " + str(xString * 10))
 print("Your age divded by 3 is " + str(xString // 3) + " remainder " + str(xString % 3))
